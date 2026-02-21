49579a1276d12efaa33004c4e70fe66e177660bd
Juanes <jgrisaleses@cesde.net>
2026-02-20 20:49:15 -0500

    feat: mover productos a estado local para CRUD

--- src/page/ProductList.jsx
+++ src/page/ProductList.jsx
@@ -11,18 +11,11 @@ function ProductList() {
     const [productsState, setProductsState] = useState(products);
 
     return (
-        <div className = {styles.container}>
-            <header className = {styles.header}>
-            <h1 className = {styles.title}>Lista de Productos</h1>
-            <p className = {styles.description}>Explora nuestra selección de productos!</p>
-            </header>
- 
-            <div>
-                {productsState.map((product) => (
-                <ProductCard key={product.id} name={product.name} />
-                ))}
-            </div>
-        </div>
+      <div>
+      {productsState.map((product) => (
+        <ProductCard key={product.id} name={product.name} />
+      ))}
+    </div>
     )          
 }
  
